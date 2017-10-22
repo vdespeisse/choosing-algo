deb1e8b624e06de0788e36c21ce8796a19098d39
vdespeisse <v.despeisse@gmail.com>
2017-10-22 21:46:35 +0200

    pff

--- algo_test.py
+++ algo_test.py
@@ -118,6 +118,11 @@ def run_compute(photographes,type_seance,seance_adjacente=1,seance_adjacente_2=1
     plt.show()
 def on_button_clicked(b,photographes,type_seance):
     clear_output()
+     display(HTML('''<style>
+    .widget-label { min-width: 20ex !important; }
+    .slide-container { min-width: 30ex !important; }
+    div.p-Widget { min-width: 50ex !important; }
+</style>'''))
     print('Photographes')
     display(pd.DataFrame(photographes).fillna(False))
     #generate_display_compute(photographes,type_seance)
